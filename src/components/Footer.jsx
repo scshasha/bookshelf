5c155037043564aa73aa65fa8fcf730236b46598
sivuyile.shasha <sivuyile.shasha@vml.com>
2025-04-09 22:07:38 +0200

    Update copy in footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,9 @@ const Footer = () => {
   return (
     <footer className="footer bg-light text-center text-lg-start mt-auto">
       <div className="container mx-auto text-center p-2">
-        <p>&copy; 2023 BookShelf. All rights reserved.</p>
-        <div className="flex justify-center space-x-4 mt-2">
-          <a href="#" className="text-gray-400 hover:text-white">
-            Privacy Policy
-          </a>
-          <a href="#" className="text-gray-400 hover:text-white">
-            Terms of Service
-          </a>
-        </div>
+        <p className="text-muted">
+          &copy; {new Date().getFullYear()} BookShelf.
+        </p>
       </div>
     </footer>
   );
