49a95d67ea254636b95d89be0f0cb76ec98ff764
sivuyile.shasha <sivuyile.shasha@vml.com>
2025-04-09 20:28:06 +0200

    Cleanup apps entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import fetchBooks from "./services/api-client";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import BookList from "./components/BookList";
 import useBooks from "./services/useBooks";
 
 function App() {
+  // Destructuring custom hook.
+  // useBooks is a custom hook that fetches books based on the search term.
+  // It returns an object containing books, loading state, and a function to set the search term.
   const { books, loading, setSearchTerm } = useBooks("Harry Potter");
 
   const handleSearch = (query) => {
